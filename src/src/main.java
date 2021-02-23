cb14fcd091ee1f5942872d3a53098989960c1ae7
Sebastian Pichler <s.pichler100795@gmail.com>
2021-02-23 12:45:02 +0100

    add exercise

--- src/src/main.java
+++ src/src/main.java
@@ -18,14 +18,8 @@ public class main {
             if (wort.contains("C")){
                 wort=wort.replace("C", "(");
             }
-            if (wort.contains("D")){
-                wort=wort.replace("D", "D");
-            }
             if (wort.contains("E")){
                 wort=wort.replace("E", "3");
-            }
-            if (wort.contains("F")){
-                wort=wort.replace("F", "F");
             }
             if (wort.contains("G")){
                 wort=wort.replace("G", "6");
@@ -36,56 +30,17 @@ public class main {
             if (wort.contains("I")){
                 wort=wort.replace("I", "!");
             }
-            if (wort.contains("J")){
-                wort=wort.replace("J", "J");
-            }
-            if (wort.contains("K")){
-                wort=wort.replace("K", "K");
-            }
             if (wort.contains("L")){
                 wort=wort.replace("L", "1");
             }
-            if (wort.contains("M")){
-                wort=wort.replace("M", "M");
-            }
-            if (wort.contains("N")){
-                wort=wort.replace("N", "N");
-            }
             if (wort.contains("O")){
                 wort=wort.replace("O", "0");
-            }
-            if (wort.contains("P")){
-                wort=wort.replace("P", "P");
-            }
-            if (wort.contains("Q")){
-                wort=wort.replace("Q", "Q");
-            }
-            if (wort.contains("R")){
-                wort=wort.replace("R", "R");
             }
             if (wort.contains("S")){
                 wort=wort.replace("S", "$");
             }
             if (wort.contains("T")){
                 wort=wort.replace("T", "7");
-            }
-            if (wort.contains("U")){
-                wort=wort.replace("U", "U");
-            }
-            if (wort.contains("V")){
-                wort=wort.replace("V", "V");
-            }
-            if (wort.contains("W")){
-                wort=wort.replace("W", "W");
-            }
-            if (wort.contains("X")){
-                wort=wort.replace("X", "X");
-            }
-            if (wort.contains("Y")){
-                wort=wort.replace("Y", "Y");
-            }
-            if (wort.contains("Z")){
-                wort=wort.replace("Z", "Z");
             }
             System.out.println(wort);
         }
